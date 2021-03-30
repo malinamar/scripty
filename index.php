fc3dea221758f049fbb90ab716649b121278ae4c
malinamar <70368115+malinamar@users.noreply.github.com>
2021-03-30 08:04:00 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<div> 
+  <aside class="main-sidebar">
+    <section class="sidebar">
+      <ul class="sidebar-menu">
+        <li class="header nav-small-cap text-uppercase">First Section</li>
+        <li class="active"><a href="#" title="">Second Section</a>
+          <ul class="treeview-menu scroll-highlight">
+            <li class=""><a href="#firstItem" title="">First Item</a></li>
+            <li class=""><a href="#secondItem" title="">Second Item</a></li>
+            <li class=""><a href="#thirdItem" title="">Third Item</a></li>
+          </ul>
+        </li>
+        <li class="treeview">Third Section         
+          <ul class="treeview-menu">
+            <li><a href="">Something1</a></li>
+            <li><a href="">Something2</a></li>
+          </ul>
+        </li>
+      </ul>
+    </section>
+  </aside>
+</div>
+<div>
+  <h2 id="firstItem" class="anchor-move">First Item</h2>
+</div>
+<div>
+  <h2 id="secondItem" class="anchor-move">Second Item</h2>
+</div>
+<div>
+  <h2 id="thirdItem" class="anchor-move">Third Item</h2>
+</div>
